992940bf21050d2d0618daccee40fbff6ad970af
TPedroso23 <tpedroso.23@gmail.com>
2018-04-02 18:36:17 +0100

    project setup. pod install;

---
+++ Media_LEI/Media_LEI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Media_LEI
+//
+//  Created by iClio on 02/04/18.
+//  Copyright © 2018 iClio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
